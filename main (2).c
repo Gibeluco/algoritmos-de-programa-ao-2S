71358e950a2a2e4514cb40b3c29f590c65fbde20
Gibeluco <giovannaf.beluco@gmail.com>
2025-08-06 20:53:31 -0300

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    char v1, v2, v3;
+ printf("informe o primeiro caracter:") ;
+ scanf("%c", &v1);
+ printf("informe o segundo caracter:") ;
+ scanf(" %c" , &v2);
+ printf ("informe o terceiro caracter:") ;
+ scanf (" %c" , &v3);
+ printf ("%c %c\n", v1, v2, v3) ;
+ return 0;
+}
