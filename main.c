b8accf038dc3d20226882fcfbbb1acf25fa24de6
Gibeluco <giovannaf.beluco@gmail.com>
2025-08-06 19:59:27 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int numero, quadrado;
+    printf("Informe o primeiro numero:") ;
+    scanf ("%d", &numero) ;
+    quadrado = numero * numero;
+    printf("O quadrado do numero eh: %d\n", quadrado) ;
+    return 0;
+    
+}
