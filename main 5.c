13854c4f48fe94d834fd3886b0a106d723073782
Gibeluco <giovannaf.beluco@gmail.com>
2025-08-06 21:37:27 -0300

    Add files via upload

---
+++ main 5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int num1, num2, soma, sub, mult, div, resto;
+   printf("informe o primeiro numero:");
+   scanf("%d", &num1);
+   print("informe o segundo numero:");
+   scanf("%d", &num2);
+   soma = num1 + num2;
+   sub = num1 - num2;
+   mul = num1 * num2;
+   div = num1 / num2;
+   resto = num % num2;
+   printf("soma: %d\n" , sub);
+   printf("multiplicacao: %d\n" , mult);
+   printf("divisao: %d\n", div);
+   printf("resto da divisao: %d\n", resto);
+   return 0;
+
+
+
+}
