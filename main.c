43dd258df2f005a0d025fb2407cb44edcdc343b8
Gibeluco <giovannaf.beluco@gmail.com>
2025-08-27 20:16:34 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double a, b, c, delta, x1, x2;
+    printf("Informe o valor de a:");
+    scanf("%lf", &a);
+    printf("Informe o valor de b:");
+    scanf("%lf", &b);
+    printf("Informe o valor de c;");
+    scanf("%lf", &c);
+    delta = b*b - 4*a*c;
+    if(delta < 0){
+        printf("Não existem raizes reais!\n");
+    }
+    else if(delta == 0) {
+        x1 = -b/(2*a);
+        printf("Ha apenas uma raiz real: %.2lf\n:", x1);
+    }
+    else{
+       x1 = (-b + sqrt(delta)) / (2*a);
+       x2 = (-b - sqrt(delta)) / (2*a);
+       printf("Raizes reais: x1 = %.2lf, x2 = %.2lf", x1, x2);
+
+            }
+    return 0;
+
+}
